3d37a1030268a819c25b3bd041dcb2849e8b0aa7
John Good <jrgood174@gmail.com>
2025-08-15 17:29:09 -0500

    agent/vector_db test

---
+++ zero-to-hero/deeplake_vectorstore.py
@@ -0,0 +1,107 @@
+#%%
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.vectorstores import DeepLake
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import RetrievalQA
+from langchain.agents import initialize_agent, Tool
+from langchain.agents import AgentType
+from dotenv import load_dotenv
+import os
+#%%
+load_dotenv(override=True)
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+ACTIVELOOP_TOKEN = os.getenv("ACTIVELOOP_TOKEN")
+
+#%%
+
+# instantiate the LLM and embeddings models
+llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0)
+embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+
+# create our documents
+texts = [
+    "Napoleon Bonaparte was born in 15 August 1769",
+    "Louis XIV was born in 5 September 1638"
+]
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+docs = text_splitter.create_documents(texts)
+
+# create Deep Lake dataset
+# TODO: use your organization id here. (by default, org id is your username)
+my_activeloop_org_id = "jrgood174" 
+my_activeloop_dataset_name = "langchain_course_from_zero_to_hero"
+dataset_path = f"hub://{my_activeloop_org_id}/{my_activeloop_dataset_name}"
+db = DeepLake(dataset_path=dataset_path, embedding_function=embeddings)
+
+# add documents to our Deep Lake dataset
+db.add_documents(docs)
+
+#%%
+## Retriever Pipeline
+retrieval_qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=db.as_retriever()
+)
+# %%
+
+tools = [
+    Tool(
+        name="Retrieval QA System",
+        func=retrieval_qa.run,
+        description="Useful for answering questions."
+    ),
+]
+
+agent = initialize_agent(
+    tools,
+    llm,
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    verbose=True
+)
+
+response = agent.run("When was Napolean born?")
+
+print(response)
+
+#%%
+
+db = DeepLake(dataset_path=dataset_path, embedding_function=embeddings)
+
+texts = [
+    "Lady Gaga was born in 28 March 1986",
+    "Michael Jeffrey Jordan was born in 17 February 1963"
+]
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=0)
+docs = text_splitter.create_documents(texts)
+
+db.add_documents(docs)
+# %%
+
+llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0)
+
+retrieval_qa=RetrievalQA.from_chain_type(
+    llm=llm,chain_type='stuff',retriever=db.as_retriever()
+)
+
+tools = [
+    Tool(
+        name="Retrieval QA System",
+        func=retrieval_qa.run,
+        description="Useful for answering questions."
+    ),
+]
+
+agent = initialize_agent(
+    tools,
+    llm,
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    verbose=True
+)
+
+response = agent.run("when was Michael Jordan born?")
+
+print(response)
